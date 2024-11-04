0de8af0b3703a6a5706f9aa1ef8898134931192e
quannd <dqdev204@gmail.com>
2024-11-04 16:51:00 +0700

    code

---
+++ main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'duantotnghiep'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database');
+});
+
+app.get('/api/data', (req, res) => {
+    db.query('SELECT * FROM your_table', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
